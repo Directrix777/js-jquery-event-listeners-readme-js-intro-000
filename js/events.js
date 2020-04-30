23fb8359a3c4f203d43f7388701d3752a3fe17af
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-30 21:48:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -9,7 +9,7 @@ function getIt()
 
 function srcIt()
 {
-  $('img').addSource = "https://s3.amazonaws.com/after-school-assets/minion-tongue.jpg"
+  $('img').attr('source', "https://s3.amazonaws.com/after-school-assets/minion-tongue.jpg")
 }
 
 function frameIt()
